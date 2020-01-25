708c5d6dd5ccc807349a5f5b77fd5fa4bdd72fd4
youhei sato <s.y.92.jr@gmail.com>
2020-01-25 13:49:03 +0900

    Lesson7　課題提出　キューブの接触時に効果音をつけましょう
    CubeControllerにブロックとブロックが触れた時音が出るように設定

---
+++ Assets/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+        // キューブの移動速度
+        private float speed = -12;
+
+        // 消滅位置
+        private float deadLine = -10;
+
+        private AudioSource block;
+        
+        // Use this for initialization
+        void Start(){
+
+                block = GetComponent<AudioSource>();
+        }
+
+        // Update is called once per frame
+        void Update () {
+                // キューブを移動させる
+                transform.Translate (this.speed* Time.deltaTime, 0, 0);
+
+
+                // 画面外に出たら破棄する
+                if (transform.position.x < this.deadLine){
+                        Destroy (gameObject);
+                }
+        }
+
+        void OnCollisionEnter2D(Collision2D collision) {
+                if (tag == "cube") {
+                        block.PlayOneShot(block.clip);
+                }
+        }
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanController : MonoBehaviour {
+        //アニメーションするためのコンポーネントを入れる
+        Animator animator;
+
+        //Unityちゃんを移動させるコンポーネントを入れる
+        Rigidbody2D rigid2D;
+
+        // 地面の位置
+        private float groundLevel = -3.0f;
+
+        // ジャンプの速度の減衰
+        private float dump = 0.8f;
+
+        // ジャンプの速度
+        float jumpVelocity = 20;
+
+        // ゲームオーバになる位置（追加）
+        private float deadLine = -9;
+
+
+        // Use this for initialization
+        void Start () {
+                // アニメータのコンポーネントを取得する
+                this.animator = GetComponent<Animator> ();
+                // Rigidbody2Dのコンポーネントを取得する
+                this.rigid2D = GetComponent<Rigidbody2D> ();
+
+        }
+        // Update is called once per frame
+        void Update () {
+                // 走るアニメーションを再生するために、Animatorのパラメータを調節する
+                this.animator.SetFloat("Horizontal", 1);
+
+                // 着地しているかどうかを調べる
+                bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+                this.animator.SetBool ("isGround", isGround);
+
+                GetComponent<AudioSource> ().volume = (isGround) ? 1:0;
+
+                if (Input.GetMouseButton (0) && isGround) {
+
+                	this.rigid2D.velocity = new Vector2 (0, this.jumpVelocity);
+                }
+
+                if (Input.GetMouseButton (0) == false) {
+                	if (this.rigid2D.velocity.y > 0) {
+                		this.rigid2D.velocity *= this.dump;
+                	}
+                }
+
+                if (transform.position.x < this.deadLine){
+                        GameObject.Find("Canvas").GetComponent<UIController> ().GameOver ();
+
+                        Destroy (gameObject);
+                }
+        }
+}
